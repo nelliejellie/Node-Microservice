f17aab0bae8f8a209723c2bf506b3fda80dd05af
nelliejellie <emekaewelike@gmail.com>
2025-07-08 08:33:48 +0100

    first commit

---
+++ books/books.js
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("List of books");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
